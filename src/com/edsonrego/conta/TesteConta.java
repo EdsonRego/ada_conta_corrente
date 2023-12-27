2272167dce8eeb8db6058882297b92954f193990
EdsonRego <edsonxrego@gmail.com>
2023-12-27 11:51:04 -0300

    Saques sem checagem de saldo

--- src/com/edsonrego/conta/TesteConta.java
+++ src/com/edsonrego/conta/TesteConta.java
@@ -5,15 +5,26 @@ import com.edsonrego.conta.Conta;
 public class TesteConta {
     public static void main(String[] args) {
 
-        Conta contaDaMaria = new Conta("Maria", "123", 100.00);
+        Conta contaDaMaria = new Conta("Maria", "123", 100.0);
 
-        sacar(20, contaDaMaria);
+        Conta contaDoJose = new Conta("Jose", "456", 30.0);
 
-        System.out.println(contaDaMaria.saldo);
-        
+        sacar(20.0, contaDaMaria);
+
+        transferir(35.0, contaDaMaria, contaDoJose);
+
+        pagar(55.0, contaDaMaria);
+
+        System.out.println(contaDaMaria.saldo); // -10.0
     }
-
-    public static void sacar(double valor, Conta conta){
+    public static void sacar(double valor, Conta conta) {
+        conta.saldo -= valor;
+    }
+    public static void transferir(double valor, Conta contaOrigem, Conta contaDestino) {
+        contaOrigem.saldo -= valor;
+        contaDestino.saldo += valor;
+    }
+    public static void pagar(double valor, Conta conta) {
         conta.saldo -= valor;
     }
 }
